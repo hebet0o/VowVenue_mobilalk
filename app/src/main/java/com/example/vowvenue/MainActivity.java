a5709a15368ac8ac8c048acef38ac71193dbb47c
hebet0o <gaboriszak46@gmail.com>
2025-04-12 12:04:05 +0200

    firebase authentikacio login, register mukodik, todo: landscape views

--- app/src/main/java/com/example/vowvenue/MainActivity.java
+++ app/src/main/java/com/example/vowvenue/MainActivity.java
@@ -5,18 +5,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
 
     EditText emailET;
     EditText passET;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +36,46 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-
-            emailET = findViewById(R.id.editTextEmailAddress);
-            passET = findViewById(R.id.editTextPassword);
-
             return insets;
         });
+        emailET = findViewById(R.id.editTextEmailAddress);
+        passET = findViewById(R.id.editTextPassword);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    public void StartShopping(){
+        Intent inti = new Intent(this, VenueActivity.class);
+        startActivity(inti);
+    }
 
     public void login(View view) {
         String printnameStr = emailET.getText().toString();
         String printpassStr = passET.getText().toString();
 
-        Log.i(LOG_TAG, "Bejelentkezett " + printnameStr + " jelsz: " + printpassStr);
+        mAuth.signInWithEmailAndPassword(printnameStr, printpassStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(LOG_TAG, "User logged in.");
+                    StartShopping();
+                } else{
+                    Log.d(LOG_TAG, "User can't be logged in.");
+                    Toast.makeText(MainActivity.this, "Something went wrong" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //Log.i(LOG_TAG, "Bejelentkezett " + printnameStr + " jelsz: " + printpassStr);
     }
 
     public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
 
+        startActivity(intent);
+    }
+
+    public void guestLogin(View view) {
+        Intent intent = new Intent(this, VenueActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/vowvenue/RegisterActivity.java
+++ app/src/main/java/com/example/vowvenue/RegisterActivity.java
@@ -1,20 +1,35 @@
 package com.example.vowvenue;
 
 import android.content.Intent;
+import android.icu.text.LocaleDisplayNames;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+
+    EditText nameET;
     EditText emailET;
     EditText passET;
+    EditText passConfirmET;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +41,49 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        emailET = findViewById(R.id.editTextText2);
+        nameET = findViewById(R.id.editTextText);
+        passET = findViewById(R.id.editTextTextPassword);
+        passConfirmET = findViewById(R.id.editTextTextPassword2);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void backToLogin(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void StartShopping(){
+        Intent inti = new Intent(this, VenueActivity.class);
+        startActivity(inti);
+    }
+
+    public void firebaseReg(View view) {
+        String email = emailET.getText().toString();
+        String pass = passET.getText().toString();
+        String passConfirm = passConfirmET.getText().toString();
+        String name = nameET.getText().toString();
+
+        if(!pass.equals(passConfirm)){
+            Log.e(LOG_TAG, "Nem egyezik a két jelszó");
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(LOG_TAG, "User created");
+                    StartShopping();
+                } else{
+                    Log.d(LOG_TAG, "User cannot be created");
+                    Toast.makeText(RegisterActivity.this, "User cannot be registered: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //Log.i(LOG_TAG, "Regisztrált email:" + email + " pass: " + pass + " confirmPass: " + passConfirm + " name: " + name);
+
     }
 }
